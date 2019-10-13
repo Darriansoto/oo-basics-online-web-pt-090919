76a330309e6a2aec2934b117a0671123fdaa95db
Darrian Soto <ddog350@gmail.com>
2019-10-13 15:22:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,37 +1,12 @@
 class Book
   
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
   def initialize(title)
     @title = title
   end
 
-  def title
-    @title
-  end
-  
-  def author=(authors_name)
-    @author = authors_name
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(number_of_pages)
-    @page_count = number_of_pages
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre 
-    @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
