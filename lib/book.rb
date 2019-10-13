da8f7cffbbe2ea81511eade999a691f27cf49c0a
Darrian Soto <ddog350@gmail.com>
2019-10-13 15:19:37 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,14 +8,32 @@ class Book
     @title
   end
   
-  def author=(author)
-    @author = author
+  def author=(authors_name)
+    @author = authors_name
   end
   
   def author
     @author
   end
   
+  def page_count=(number_of_pages)
+    @page_count = number_of_pages
+  end
   
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre 
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page... wow, you read fast!"
+  end
   
 end
